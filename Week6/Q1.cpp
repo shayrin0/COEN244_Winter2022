09130bd29fffcb6d89d126cdf3e3e85cf1b5cd0f
shayrin0 <shayrin.panahi@gmail.com>
2022-02-18 16:17:39 -0500

    add question 1 and 2 week 6

---
+++ Week6/Q1.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student {
+protected:
+	string fName;
+	string lName;
+	int id;
+
+public:
+	Student(int id, string fname, string lname)
+	{
+	}
+
+	void setId(int id)
+	{
+	}
+
+	int getId()
+	{
+	}
+
+	void setFname(string fname)
+	{
+	}
+
+	string getFname()
+	{
+	}
+
+	void setLname(string lname)
+	{
+	}
+
+	string getLname()
+	{
+	}
+
+	void printInformation() 
+	{
+	}
+
+	~Student(){}
+
+};
+
+class UndergraduateStudent : public Student {
+
+protected:
+	string status;
+public:
+	UndergraduateStudent(int id, string fname, string lname, string status)
+	{
+	}
+
+	void setStatus(string status)
+	{
+	}
+
+	string getStatus()
+	{
+	}
+
+	 void printInformation() 
+	{
+	}
+
+	~UndergraduateStudent(){}
+
+};
+
+class GraduateStudent : public Student {
+
+protected:
+	string status;
+
+public:
+	GraduateStudent(int id, string fname, string lname, string status)
+	{
+	}
+
+	void setStatus(string status)
+	{
+	}
+
+	string getStatus()
+	{
+	}
+
+	void printInformation()
+	{
+	}
+
+	~GraduateStudent(){}
+
+};
+
+int main()
+{
+
+	return 0;
+}
