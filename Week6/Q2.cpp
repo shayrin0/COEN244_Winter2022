e03b2c563170ce012e9952f693f231819c824761
shayrin0 <shayrin.panahi@gmail.com>
2022-02-18 17:16:00 -0500

    update question 2

--- Week6/Q2.cpp
+++ Week6/Q2.cpp
@@ -4,48 +4,59 @@ using namespace std;
 
 class Student {
 protected:
-	string fName;
-	string lName;
+	string fname;
+	string lname;
 	int id;
 
 public:
-
-	Student(){}
-
+    Student() {}
+    
 	Student(int id, string fname, string lname)
 	{
+        this->id = id; // setId(id)
+        this->fname = fname;
+        this->lname = lname;
 	}
 
 	void setId(int id)
 	{
+        this->id = id;
+//        this->id = id;
 	}
 
 	int getId()
 	{
+        return id;
 	}
 
 	void setFname(string fname)
 	{
+        this->fname = fname;
 	}
 
 	string getFname()
 	{
+        return fname;
 	}
 
 	void setLname(string lname)
 	{
+        this->lname = lname;
 	}
 
 	string getLname()
 	{
+        return lname;
 	}
+    
+    void print(Student *s)
+    {
+        s->printInformation();
+    }
 
-	void print()
+	virtual void printInformation() 
 	{
-	}
-
-	virtual void printInformation()
-	{
+        cout << "Student ID: " << id << "\nFirstname: " << fname << "\nLastname: " << lname << endl;
 	}
 
 	~Student(){}
@@ -56,24 +67,31 @@ class UndergraduateStudent : public Student {
 
 protected:
 	string status;
+    
 public:
-	UndergraduateStudent(int id, string fname, string lname, string status)
-	{
+	UndergraduateStudent(int id, string fname, string lname, string status) : Student(id, fname, lname)
+	{   
+        this->status = status;
 	}
 
 	void setStatus(string status)
 	{
+        this->status = status;
 	}
 
 	string getStatus()
 	{
+        return status;
 	}
 
 	 void printInformation() 
 	{
+        cout << "Student ID: " << id << "\nFirstname: " << fname << "\nLastname: " << lname << 
+                "\nStudent status: " << status << endl;
 	}
 
 	~UndergraduateStudent(){}
+
 };
 
 class GraduateStudent : public Student {
@@ -82,27 +100,63 @@ protected:
 	string status;
 
 public:
-	GraduateStudent(int id, string fname, string lname, string status)
+	GraduateStudent(int id, string fname, string lname, string status) : Student(id, fname, lname)
 	{
+        this->status = status;
 	}
 
 	void setStatus(string status)
 	{
+        this->status = status;
 	}
 
 	string getStatus()
 	{
+        return status;
 	}
 
 	void printInformation()
 	{
+        cout << "Student ID: " << id << "\nFirstname: " << fname << "\nLastname: " << lname << 
+                "\nStudent status: " << status << endl;
 	}
 
 	~GraduateStudent(){}
+
 };
 
 int main()
 {
+	UndergraduateStudent* s2 = new UndergraduateStudent(456, "dany", "wilson","Undergraduate");
+
+	GraduateStudent * s3 = new GraduateStudent(123, "sami", "omar", "Graduate");
+
+	Student* obj = new Student();
+
+	obj->print(s2);
+
+	obj->print(s3);
+    // which printInformation function will be called?
+//	Student *s1 = new Student(123, "jone", "smith");
+//    s1->printInformation();
+//
+//    Student* s2 = new UndergraduateStudent(456, "ahmad", "sami","Undergraduate");
+//    s2->printInformation();
+//    
+////    UndergraduateStudent ugs;
+//
+//    Student* s3 = new GraduateStudent(456, "rose", "jonson", "Graduate");
+//    s3->printInformation();
+//    
+//    
+//    Student *s4;
+//    UndergraduateStudent ugs2(1234, "koa", "panahi","Undergraduate");
+//    s4 = &ugs2;
+//    
+//    s4->printInformation();
+    
+//    UndergraduateStudent *ugs = new UndergraduateStudent(456, "rose", "jonson", "Undergraduate");
+//    ugs->printInformation();
 
 	return 0;
 }
